8dd4be30cc48e01117973a5bc1f25d273d1582e4
D <22173829+kagekiyo7@users.noreply.github.com>
2025-07-15 03:11:15 +0900

    Update remap_SO505i.py

--- remap_SO505i.py
+++ remap_SO505i.py
@@ -5,12 +5,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description="SO505i FTL Remapper")
 parser.add_argument("input", help="A file with the same name and the .oob extension must be present in the same folder.")
-parser.add_argument("-o", "--output")
+parser.add_argument("-o", "--output", default=None)
 args = parser.parse_args()
 
 def detect_sector_per_block(oob_data):
-    count = 0
-
     start = 0 
     for i in range(len(oob_data) // 0x10):
         if oob_data[(i*0x10) + 0xB : (i*0x10) + 0xD] != b"\xFF" * 2:
@@ -18,10 +16,10 @@ def detect_sector_per_block(oob_data):
         else:
             start += 1
 
-
+    count = 0
     for i in range(start, len(oob_data) // 0x10):
         block_id = (int.from_bytes(oob_data[(i*0x10) + 0xB : (i*0x10) + 0xD], "big") & 0x0FFF) >> 1
-        
+
         if count == 0:
             reference_block_id = block_id
             count += 1
@@ -39,13 +37,11 @@ def main():
     with open(re.sub(r"\.bin$", ".oob", args.input), "rb") as inf:
         oob_data = inf.read()
 
-    if args.output is not None:
-        output = args.output
-    else:
-        output = os.path.join(
+
+    output = args.output or os.path.join(
             os.path.dirname(args.input),
             f"{os.path.splitext(os.path.basename(args.input))[0]}_remapped.bin"
-        )
+    )
 
     sector_per_block = detect_sector_per_block(oob_data)
     if sector_per_block == 0: raise ValueError("Failure to detect a sector_per_block.")
